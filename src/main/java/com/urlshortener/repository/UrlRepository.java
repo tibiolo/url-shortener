a296c9d3ff8ae899644a1fa7503c8d462bbb09c5
maks <makso544g@gmail.com>
2026-08-12 01:17:02 +0100

    Added UrlRepository query methods

--- src/main/java/com/urlshortener/repository/UrlRepository.java
+++ src/main/java/com/urlshortener/repository/UrlRepository.java
@@ -4,8 +4,12 @@ import com.urlshortener.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, UUID> {
+    Optional<Url> findByShortCode(String shortCode);
+
+    boolean existsByShortCode(String shortCode);
 }
